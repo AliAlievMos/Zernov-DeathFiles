85a69d0a56c6285815a625ccbd7b8fa11bb8f3ed
Ali Aliev <ali.aliev.msk>
2022-11-27 19:14:51 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+
+function showIt() {
+	document.getElementById("hid").style.visibility = "visible";
+}
+setTimeout("showIt()", 6000); // after 3 seconds
---
+++ scriptElse.js
@@ -0,0 +1,5 @@
+
+function showIt() {
+	document.getElementById("hid").style.visibility = "visible";
+}
+setTimeout("showIt()", 1000); // after 3 seconds
